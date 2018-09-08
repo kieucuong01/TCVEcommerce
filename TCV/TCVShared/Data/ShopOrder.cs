8964e6ad111f671849b1126b09f73aa7ec50f9e7
vuongth <tadlhvuong@gmail.com>
2018-09-08 09:13:50 +0700

    d

---
+++ TCV/TCVShared/Data/ShopOrder.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace TCVShared.Data
+{
+    public class ShopAddress
+    {
+        public int Id { get; set; }
+
+        public int? UserId { get; set; }
+
+        [Display(Name = "Điện thoại")]
+        public string Phone { get; set; }
+
+        [StringLength(256)]
+        [Display(Name = "Địa chỉ")]
+        [DataType(DataType.MultilineText)]
+        public string Address { get; set; }
+
+        [StringLength(64)]
+        [Display(Name = "Thành phố")]
+        public string City { get; set; }
+
+        [StringLength(64)]
+        [Display(Name = "Tiểu bang")]
+        public string State { get; set; }
+
+        [StringLength(64)]
+        [Display(Name = "Quốc gia")]
+        public string Country { get; set; }
+
+        [StringLength(32)]
+        [Display(Name = "Mã zip")]
+        public string ZipCode { get; set; }
+
+        [ForeignKey("UserId")]
+        public virtual AppUser AppUser { get; set; }
+    }
+
+    [Table("ShopOrderItems")]
+    public class OrderItem
+    {
+        public int Id { get; set; }
+
+        public int OrderId { get; set; }
+
+        public int ItemId { get; set; }
+
+        [Display(Name = "Số lượng")]
+        public int Quantity { get; set; }
+
+        [Display(Name = "Thuộc tính")]
+        public string ItemAttrib { get; set; }
+
+        [ForeignKey("OrderId")]
+        public virtual ShopOrder ShopOrder { get; set; }
+
+        [ForeignKey("ItemId")]
+        public virtual ShopItem ShopItem { get; set; }
+    }
+
+    public class ShopOrder
+    {
+        public int Id { get; set; }
+
+        public int? UserId { get; set; }
+
+        [Display(Name = "Thanh toán")]
+        public int? BillingId { get; set; }
+
+        [Display(Name = "Giao hàng")]
+        public int? ShippingId { get; set; }
+
+        [Display(Name = "Chỉnh giá")]
+        public double AdjustPrice { get; set; }
+
+        [Display(Name = "Phí giao hàng")]
+        public double ShippingFee { get; set; }
+
+        [Display(Name = "Tổng cộng")]
+        public double GrandTotalPrice { get; set; }
+
+        [Display(Name = "Thanh toán")]
+        public string PaymentInfo { get; set; }
+
+        [Display(Name = "Ngày tạo")]
+        public DateTime? CreateTime { get; set; }
+
+        [Display(Name = "Ngày giao")]
+        public DateTime? DeliveryTime { get; set; }
+
+        [StringLength(256)]
+        [Display(Name = "Ghi chú")]
+        [DataType(DataType.MultilineText)]
+        public string Note { get; set; }
+
+        [Display(Name = "Trạng thái Đơn hàng")]
+        public OrderStatus OrderStatus { get; set; }
+
+        [Display(Name = "Trạng thái Thanh toán")]
+        public PaymentStatus PaymentStatus { get; set; }
+
+        [ForeignKey("UserId")]
+        public virtual AppUser AppUser { get; set; }
+
+        [ForeignKey("BillingId")]
+        public virtual ShopAddress Billing { get; set; }
+
+        [ForeignKey("ShippingId")]
+        public virtual ShopAddress Shipping { get; set; }
+
+        [Display(Name = "Sản phẩm")]
+        public virtual ICollection<OrderItem> Items { get; set; }
+    }
+}
---
+++ TCV/TCVWeb/Controllers/AccountController.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using TCVShared.Data;
+
+namespace TCVWeb.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+        private readonly ILogger<AccountController> _logger;
+        public AccountController(
+            UserManager<AppUser> userManager,
+            SignInManager<AppUser> signInManager,
+            ILogger<AccountController> logger)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _logger = logger;
+        }
+
+        [TempData]
+        public string ErrorMessage { get; set; }
+
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Account/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Account/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Account/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Account/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Account/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Account/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Account/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
