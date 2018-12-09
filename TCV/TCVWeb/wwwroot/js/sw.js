23155d173f396ffe19e06ce4b924eacdb33be549
Cuong Kieu <kieucuong01@gmail.com>
2018-12-09 12:28:40 +0700

    + Clear sw.js file

--- TCV/TCVWeb/wwwroot/js/sw.js
+++ TCV/TCVWeb/wwwroot/js/sw.js
@@ -16,30 +16,3 @@
  *  limitations under the License
  *
  */
-const version = "0.0.1";
-const cacheName = `ticivi-${version}`;
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(cacheName).then(cache => {
-      return cache.addAll([
-        `/`,
-        `/index.html`
-      ])
-          .then(() => self.skipWaiting());
-    })
-  );
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(self.clients.claim());
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.open(cacheName)
-      .then(cache => cache.match(event.request, {ignoreSearch: true}))
-      .then(response => {
-      return response || fetch(event.request);
-    })
-  );
-});
