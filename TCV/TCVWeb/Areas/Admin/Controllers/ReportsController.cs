b0f255a8cad15a7a55be76f921310fc154173a25
HungVuongTran <tadlhvuong@gmail.com>
2019-01-22 22:48:01 +0700

    xxx

---
+++ TCV/TCVWeb/Areas/Admin/Controllers/ReportsController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using TCVShared.Data;
+using TCVShared.Helpers;
+
+namespace TCVWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ReportsController : Controller
+    {
+        private readonly ILogger _logger;
+        private readonly AppDBContext _dbContext;
+
+        public ReportsController(
+            ILogger<ReportsController> logger,
+            AppDBContext dbContext)
+        {
+            _logger = logger;
+            _dbContext = dbContext;
+        }
+        // GET: Reports
+        public ActionResult General(PagedList<OrderItem> model)
+        {
+            //var filterQuery = _dbContext.OrderItems.Where(x => model.Search == null || x.SKU.Contains(model.Search));
+            //if (model.Filter == null)
+            //    filterQuery = filterQuery.Where(x => x.Status != EntityStatus.Disabled);
+            //else
+            //{
+            //    if (int.TryParse(model.Filter, out int status))
+            //    {
+            //        EntityStatus etStatus = (EntityStatus)status;
+            //        filterQuery = filterQuery.Where(x => x.Status == etStatus);
+            //    }
+            //}
+            var filterQuery = _dbContext.OrderItems.ToList();
+            var selectQuery = filterQuery.OrderByDescending(x => x.Id).Skip((model.CurPage - 1) * model.PageSize).Take(model.PageSize);
+
+            model.TotalRows = filterQuery.Count();
+            model.Content = selectQuery.ToList();
+
+            return View(model);
+        }
+
+        // GET: Reports/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Reports/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Reports/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(General));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Reports/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Reports/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(General));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Reports/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Reports/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(General));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
